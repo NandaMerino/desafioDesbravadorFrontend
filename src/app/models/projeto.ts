0221e631f83b70928fae325bbd58c06475a39791
Fernanda Merino <fernanda.merino@gmail.com>
2024-10-09 18:30:30 -0300

    correções models

--- src/app/models/projeto.ts
+++ src/app/models/projeto.ts
@@ -3,21 +3,21 @@ import { Pessoa } from "./pessoa";
 export class Projeto {
     id!: number;
     nome!: string;
-    datainicio!: Date;
-    dataprevisaofim!: Date;
-    datafim!: Date;
+    dataInicio!: Date;
+    dataPrevisaoFim!: Date;
+    dataFim!: Date;
     descricao!: string;
     status!: string;
     orcamento!: number;
     risco!: string;
     idgerente!: Pessoa;
 
-    constructor(id: number, nome: string, datainicio: Date, dataprevisaofim: Date, datafim: Date, descricao: string, status: string, orcamento: number, risco: string, gerente: Pessoa){
+    constructor(id: number, nome: string, dataInicio: Date, dataPrevisaoFim: Date, dataFim: Date, descricao: string, status: string, orcamento: number, risco: string, gerente: Pessoa){
         this.id = id;
         this.nome = nome;
-        this.datainicio = datainicio;
-        this.dataprevisaofim = dataprevisaofim;
-        this.datafim = datafim;
+        this.dataInicio = dataInicio;
+        this.dataPrevisaoFim = dataPrevisaoFim;
+        this.dataFim = dataFim;
         this.descricao = descricao;
         this.status = status;
         this.orcamento = orcamento;
